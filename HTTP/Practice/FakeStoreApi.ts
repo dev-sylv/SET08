f874b6b0e99c6e6b3fb218ef84856253c8333eb6
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-09-10 20:57:39 +0100

    product images and files saved

--- HTTP/Practice/FakeStoreApi.ts
+++ HTTP/Practice/FakeStoreApi.ts
@@ -23,8 +23,59 @@ const Server = http.createServer(
       data: null,
     };
     try {
-      // When you hit a route it will give you all the details of the products.
+      //1. When you hit a route it will give you all the details of the products.
       if (method === "GET" && url === "/getallproducts") {
+        let requestBody = "";
+
+        req.on("data", (chunk) => {
+          requestBody += chunk;
+        });
+        req.on("end", async () => {
+          let requestData = JSON.parse(requestBody);
+
+          const productendpoint = await axios.get(
+            `https://fakestoreapi.com/products`
+          );
+
+          if (productendpoint.status) {
+            const productDetails = productendpoint.data;
+
+            // 2. Download all the images of the products. and place into a folder.
+            const productImages = productDetails.avatar_url;
+            const avatarfilename = `${productDetails.image}.jpg`;
+            const avatarfolder = path.join(
+              __dirname,
+              "Product_Images",
+              avatarfilename
+            );
+
+            const getavatarurl = await axios.get(productImages, {
+              responseType: "stream",
+            });
+
+            getavatarurl.data.pipe(fs.createWriteStream(avatarfolder));
+
+            // 3. Save the title of each of the products in a "Txt File"
+
+            status = 200;
+
+            (response.message = `All ${productDetails.length} products gotten successfully`),
+              (response.sucess = true),
+              (response.data = productDetails);
+
+            res.write(JSON.stringify({ status, response }));
+            res.end();
+          } else {
+            status = 404;
+
+            (response.message = "Products not found"),
+              (response.sucess = false),
+              (response.data = null);
+
+            res.write(JSON.stringify({ status, response }));
+            res.end();
+          }
+        });
       } else {
         (response.message = "Check your Routes"),
           (response.sucess = false),
